c54c8fc29b492a733d5c980d58cc24179a632dc2
Andrei Gavrilov <ddarksidee@gmail.com>
2023-07-08 01:05:11 +0200

    adding c++

---
+++ DudesGMTK2023/Source/DudesGMTK2023/DudesGMTK2023.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DudesGMTK2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DudesGMTK2023, "DudesGMTK2023" );
